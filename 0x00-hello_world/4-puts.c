7442a6ab2b5df8de6d7a819235535614c709849b
mark30 <mac4tec@gmail.com>
2022-10-13 16:04:10 -0700

    first c files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
