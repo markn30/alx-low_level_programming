58b4eaf087bf9e9a3cc3b48c64c0b465fa56ac6d
mark30 <mac4tec@gmail.com>
2022-10-13 19:14:09 -0700

    last checks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-/**                                                            * main -Entry point
+/*                                                             *main -Entry point
  *
  * Return: Always 1 (success)                                  */                                                           int main(void)
 {                                                                     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);                                              return (1);
