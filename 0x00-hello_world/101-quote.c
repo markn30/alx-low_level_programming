46352e571f2d9845bbc21624fc2a8738cf819d3f
mark30 <mac4tec@gmail.com>
2022-10-13 19:35:04 -0700

    -

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,11 @@
 #include <unistd.h>
 
 /**
- *main - Entry point
- *
- *Return: Always 1 (Success)
+ * main - prints exactly "and that piece of art is useful"
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	char *mgs = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
-
-	write(STDOUT_FILENO, mgs, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
