e156f71e548074ff37662d9c771a3c3e134d25ae
mark30 <mac4tec@gmail.com>
2022-10-18 10:34:24 -0700

    corrections to if clause' git push git add 5-sign.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,11 @@
-#include"main.h"
+#include "main.h"
 
 /**
- *_isalpha - checks if character is a letter
- *both lowercase or uppercase
- *c: takes input from other functions.
- *
- *Return: 1 is c if true else 0
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
-
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 && c >= 65 && c >= 90)
-		return (1);
-		return (0);
-}
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}}
