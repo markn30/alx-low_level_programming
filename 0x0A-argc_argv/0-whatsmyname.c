c9e461015684d88b19ffabe9167a206ad7dcd321
mark30 <mac4tec@gmail.com>
2022-11-04 12:04:55 -0700

    0-corrrctions

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,12 @@
  * @argv: an array of a command listed
  * Return: 0 for success
  */
-int main(int argc __attribute__((unused)), char *argv[])
+
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
