ea962207c90d7e8b907ceb18521ebaf81e7fb650
mark30 <mac4tec@gmail.com>
2022-11-04 12:36:27 -0700

    0ed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include "main.h"
-/**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- * Return: Always 0 (Success)
- */
-int main(int argc, char *argv[])
-{
-	(void) argc;
-	printf("%s\n", argv[0]);
+#include <main.h>
 
+/**
+ * main - this is a function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
 	return (0);
 }
