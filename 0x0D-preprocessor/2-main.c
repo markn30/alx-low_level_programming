0e7640321d53898df09183b5ad90749e782e82e7
mark30 <mac4tec@gmail.com>
2022-11-14 15:25:02 -0800

    task 0 - task 4 completed, tests

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled fro and a new line
+ *
+ * Return: Always Success (0)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
